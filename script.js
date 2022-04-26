34826b2a868502cb880fc6b6b6564bc81462a8f8
Juliya Sartasova <anjee2007@mail.ru>
2022-04-26 13:18:00 +0300

    выполнила практику 2

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastOfSeen = +prompt('Один из последних просмотренных фильмов?', ''),
-      ratingOfFilm = +prompt('На сколько оцените его?', ''),
-      lastOfSeen2 = +prompt('Один из последних просмотренных фильмов?', ''),
-      ratingOfFilm2 = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastOfSeen] = ratingOfFilm;
-personalMovieDB.movies[lastOfSeen2] = ratingOfFilm2;
 
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    let lastOfSeen = prompt('Один из последних просмотренных фильмов?', ''),
+        ratingOfFilm = prompt('На сколько оцените его?', '');
+
+    if (lastOfSeen !== '' && ratingOfFilm !== ''
+        && lastOfSeen != null && ratingOfFilm != null
+        && lastOfSeen.length < 50 && ratingOfFilm.length < 50) {
+        personalMovieDB.movies[lastOfSeen] = ratingOfFilm;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('There are few films seen');
+} else if (10 <= personalMovieDB.count < 30) {
+    alert('You are a classic spectator');
+} else if (personalMovieDB.count >= 30) {
+    alert('You are a cinephile');
+} else {
+    alert('Error');
+}
+
